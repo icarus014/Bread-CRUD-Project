7890ca8c55678705983c01331e837bf840ee9be8
Darian Brown <browndarian011@gmail.com>
2023-03-04 13:26:20 -0500

    Custom hepler methods

--- models/breads.js
+++ models/breads.js
@@ -3,10 +3,20 @@ const { Schema } = mongoose
 
 
 const breadSchema = new Schema({
-  name: { type: String,required: true },
-  hasGluten: { type: Boolean },
-  image: { type: String }
+  name: { type: String, required: true },
+  hasGluten: Boolean,
+  image: { type: String, default: 'http://placehold.it/500x500.png' },
+  baker: {
+    type: String,
+    enum: ['Rachel', 'Monica', 'Joey', 'Chandler', 'Ross', 'Phoebe']
+  }
 })
+
+// HELPER METHODS
+breadSchema.methods.getBakedBy = function(){
+  return `${this.name} was baked with love by ${this.baker}`
+}
+
 
 
 const Bread = mongoose.model('Bread', breadSchema)
